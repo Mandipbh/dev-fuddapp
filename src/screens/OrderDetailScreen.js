72d99800a544e3103173442ec94e25e7f76614f3
Mandipbh <111733718+Mandipbh@users.noreply.github.com>
2023-04-03 18:47:14 +0530

    latest code

--- src/screens/OrderDetailScreen.js
+++ src/screens/OrderDetailScreen.js
@@ -442,8 +442,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    borderBottomWidth: scale(0.2),
-    paddingVertical: scale(1),
   },
   row1: {
     flexDirection: 'row',
